ad49936a0086e8c7353dda3f3ba7a4d1e4dba2d6
Ellen Dunn <edunn227@gmail.com>
2018-08-01 09:47:49 -0500

    added test

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080)
+
+if (require.main === module) {
+	app.listen(process.env.PORT || 8080, function() {
+	console.log(`App is listening on ${PORT}`);	
+	});
+}
+
+module.exports = app;
+
---
+++ test/test-server.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('index html page', function() {
+
+	it('should exist', function() {
+		return chai 
+		.request(app)
+		.get('/')
+		.then(function(res) {
+			expect(res).to.have.status(200);
+			expect(res).to.be.html;
+		})
+
+	})
+})
