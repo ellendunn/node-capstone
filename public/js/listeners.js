db3ddd7d05f94b703011f2a8bec0dd1706d447db
Ellen Dunn <edunn227@gmail.com>
2018-08-09 12:13:51 -0500

    crud fix

--- public/js/listeners.js
+++ public/js/listeners.js
@@ -1,9 +1,10 @@
 const attachListeners = (() => () => {
 
-	$('#add-app').submit(handlers.handleAppSubmit)
+	$('#app').on('submit', '#add-app', handlers.handleAppSubmit)
 	$('.collection').on('click', '.delete', handlers.handleAppDelete)
 	$('.collection').on('click', '.edit', handlers.handleGetApp)
 	$('#app').on('click', '.save-update', handlers.handleAppUpdate)
+
 
 // NAV BAR LISTENERS
 	$('#current-apps').on('click', handlers.handleGetAllApps)
